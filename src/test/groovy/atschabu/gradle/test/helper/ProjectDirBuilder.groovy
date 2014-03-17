e60b07ea6901de248a050bebf50d4fc20b182332
Andreas Tschabuschnig <atschabu@gmail.com>
2014-03-17 20:26:50 +0000

    some documentation

--- src/test/groovy/atschabu/gradle/test/helper/ProjectDirBuilder.groovy
+++ src/test/groovy/atschabu/gradle/test/helper/ProjectDirBuilder.groovy
@@ -9,6 +9,15 @@ import org.gradle.util.CollectionUtils
 import org.gradle.util.TextUtil
 import org.junit.rules.TemporaryFolder
 
+/**
+ * Helper to create a fake project within a TemporaryFolder. Each builder starts with a settings.gradle file which
+ * adds the current execution classpath to the buildscript classpath configuration. This adds all custom plugins to the
+ * test execution.
+ *
+ * For each path, the first time withFile(String) is called an empty file is created in the TemporaryFolder, but no
+ * content is written to the file itself until build() gets called. If the file already exists in the file system an
+ * exception will be thrown.
+ */
 class ProjectDirBuilder {
 
     final TemporaryFolder dir
@@ -24,8 +33,9 @@ class ProjectDirBuilder {
      * @return an instance of GradleFile which will be written to the given path
      * within the this builders TemporyFolder. Returns existing instance if a file
      * with the given path already exists.
+     * @throws IOException If a file with the same path already exists in the filesystem
      */
-    GradleFile withFile(String path) {
+    GradleFile withFile(String path) throws IOException {
         GradleFile file = files.get(path)
         if (!file) {
             file = new GradleFile(dir.newFile(path))
@@ -57,6 +67,7 @@ class ProjectDirBuilder {
     }
 
     private List<File> getClasspathAsFiles() {
+        // we only have one classpath within the JUnit context, so any classloader should do
         List<URL> classpathUrls = ClasspathUtil.getClasspath(this.class.classLoader)
         return CollectionUtils.collect(classpathUrls, new ArrayList<File>(classpathUrls.size()), new Transformer<File, URL>() {
             public File transform(URL url) {
